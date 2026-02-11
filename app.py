2b4bdfe42a9e02a19b13ad2ff7251a7daaf9311a
Arief Putra Aprianto <102644215+ariefp4@users.noreply.github.com>
2026-02-11 18:08:33 +0700

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<NotepadPlus>
-    <Session activeView="0">
-        <mainView activeIndex="0">
-            <File firstVisibleLine="138" xOffset="0" scrollWidth="719" startPos="4803" endPos="4803" selMode="0" offset="0" wrapCount="1" lang="None (Normal Text)" encoding="-1" userReadOnly="no" filename="C:\Users\Admin\Downloads\convertsoal\New Text Document.txt" backupFilePath="C:\Users\Admin\AppData\Roaming\Notepad++\backup\New Text Document.txt@2026-02-11_175842" originalFileLastModifTimestamp="1462789470" originalFileLastModifTimestampHigh="31234885" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
-        </mainView>
-        <subView activeIndex="0" />
-    </Session>
-</NotepadPlus>
